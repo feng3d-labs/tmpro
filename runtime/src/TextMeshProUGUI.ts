f595eafedd06bed0045c030c72160c6f092ee8ad
feng <908087098@qq.com>
2020-02-14 10:54:30 +0800

    TextMeshProUGUI

---
+++ runtime/src/TextMeshProUGUI.ts
@@ -0,0 +1,11 @@
+namespace TMPro
+{
+    // [0]:Text (TMP) (UnityEngine.RectTransform)
+    // [1]:Text (TMP) (UnityEngine.CanvasRenderer)
+    // [2]:Text (TMP) (TMPro.TextMeshProUGUI)
+
+    export class TextMeshProUGUI extends feng3d.Component
+    {
+
+    }
+}
